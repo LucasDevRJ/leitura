9d0b48b3dccdb885c73777a7fa23f9ba30c2fe92
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-27 18:43:30 -0300

    Adição de comentários didáticos no código

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
@@ -1,5 +1,11 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
+//Classe para adicionar Actions
 public class HelloController {
 
+	//Action que serve para requisitar uma transação para 
+	//o usuári e retorna uma view (html)
+	public String hello() {
+		return "Hello";
+	}
 }

4891a8d51ecfc1fefd755e22f96b9618f59bfa49
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-27 18:25:56 -0300

    Início do projeto Leitura

---
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/LeituraApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasdevrj.mvc.leitura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeituraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeituraApplication.class, args);
+	}
+
+}

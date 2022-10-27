1362a523e177713c778f76e5ffc22082b079afcf
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-27 19:08:23 -0300

    Trocando HttpServlet pelo Model

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.github.lucasdevrj.mvc.leitura.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //para indicar que a classe é uma controller para o Spring, assim sendo gerenciado por ele
@@ -11,8 +12,8 @@ public class HelloController { //Classe para adicionar Actions
 	//Action que serve para requisitar uma transação para 
 	//o usuári e retorna uma view (html)
 	@GetMapping("/hello") //para o Spring mapear essa Action e executa-la
-	public String hello(HttpServletRequest request) { //HttpServletRequest para acessar atributos da view
-		request.setAttribute("nome", "World");
+	public String hello(Model model) { //HttpServletRequest para acessar atributos da view
+		model.addAttribute("nome", "World");
 		return "Hello";
 	}
 }

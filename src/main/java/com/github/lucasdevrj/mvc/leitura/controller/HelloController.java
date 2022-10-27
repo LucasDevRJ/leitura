acbde7d768c05a89f787d77124017205a585d850
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-27 18:53:32 -0300

    Redirecionamento da Action no Spring

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
-//Classe para adicionar Actions
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //para indicar que a classe é uma controller para o Spring, assim sendo gerenciado por ele
+public class HelloController { //Classe para adicionar Actions
 
 	//Action que serve para requisitar uma transação para 
 	//o usuári e retorna uma view (html)
+	@GetMapping("/hello") //para o Spring mapear essa Action e executa-la
 	public String hello() {
 		return "Hello";
 	}

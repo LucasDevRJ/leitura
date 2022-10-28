7fa3ea3e2011fdfe52b6be6b5df3b1181c675027
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-28 07:49:15 -0300

    Criação da página index

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HelloController.java
@@ -1,7 +1,5 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;

9592bdcd9f41631fc8e2c206c069908d013492df
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-28 08:13:31 -0300

    Criação dos atributos do livro

--- src/main/java/com/github/lucasdevrj/mvc/leitura/model/Livro.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/model/Livro.java
@@ -1,6 +1,14 @@
 package com.github.lucasdevrj.mvc.leitura.model;
 
+import java.time.LocalDate;
+
 public class Livro {
 
-	
+	private String nome;
+	private LocalDate data;
+	private String autor;
+	private Integer paginas;
+	private String categoria;
+	private String descricao;
+	private String link;
 }

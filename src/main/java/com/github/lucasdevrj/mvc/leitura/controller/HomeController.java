f9a2f120a1ad40f128af5831068e89a175a324a4
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-28 08:06:55 -0300

    Alteração de index para home

---
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HomeController.java
@@ -0,0 +1,8 @@
+package com.github.lucasdevrj.mvc.leitura.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller //Indica para o Spring que é um controller, para ele gerenciar
+public class HomeController {
+
+}

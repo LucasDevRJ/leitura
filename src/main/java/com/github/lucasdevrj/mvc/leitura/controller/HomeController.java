aa52e28fded110228499cc4c1faadcbc43f008e9
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-28 08:11:20 -0300

    Criação da classe Livro

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HomeController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/HomeController.java
@@ -1,8 +1,17 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.github.lucasdevrj.mvc.leitura.model.Livro;
 
 @Controller //Indica para o Spring que é um controller, para ele gerenciar
 public class HomeController {
 
+	@GetMapping("/home") //Action que retorna a view home
+	public String home() {
+		Livro livro = new Livro();
+		
+		return "/home";
+	}
 }
---
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/model/Livro.java
@@ -0,0 +1,6 @@
+package com.github.lucasdevrj.mvc.leitura.model;
+
+public class Livro {
+
+	
+}

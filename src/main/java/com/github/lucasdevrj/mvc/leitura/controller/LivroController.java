14d00ae914a79cdd4aa2f9c13d6f7165754f04b2
Lucas Pereira <lucaspereiradelima2020@gmail.com>
2022-10-31 09:14:00 -0300

    Classe controller completa recebendo e enviando requisição.

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/LivroController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/LivroController.java
@@ -1,8 +1,15 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("livro")
 public class LivroController {
 
+	@GetMapping("formulario")
+	public String formulario() {
+		return "livro/formulario";
+	}
 }

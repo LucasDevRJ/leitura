95467a9b62c51b38bd0dd7214ce9bcedd4827a50
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-16 20:30:55 -0300

    Criação da classe para configurar login no site.

---
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/ConfiguracaoSegurancaWeb.java
@@ -0,0 +1,11 @@
+package com.github.lucasdevrj.mvc.leitura;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration //Indica para Spring que esta é uma classe de configuração
+@EnableWebSecurity
+public class ConfiguracaoSegurancaWeb extends WebSecurityConfigurerAdapter { //Classe para configurar login do site
+
+}

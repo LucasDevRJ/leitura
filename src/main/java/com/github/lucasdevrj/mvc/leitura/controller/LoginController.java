00723713a3426199acccefc0de1e3504239d4485
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-16 21:25:16 -0300

    Criação do Controller do login.

--- src/main/java/com/github/lucasdevrj/mvc/leitura/controller/LoginController.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/controller/LoginController.java
@@ -1,5 +1,16 @@
 package com.github.lucasdevrj.mvc.leitura.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LoginController {
 
+	@GetMapping
+	@RequestMapping("/login")
+	
+	public String login() {
+		return "login";
+	}
 }

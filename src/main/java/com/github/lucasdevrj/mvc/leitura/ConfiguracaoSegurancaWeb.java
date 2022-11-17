b6cc88feeac713bf657e4f14b31bc9001d8be9c8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-11-16 21:08:57 -0300

    Criação da página de login.

--- src/main/java/com/github/lucasdevrj/mvc/leitura/ConfiguracaoSegurancaWeb.java
+++ src/main/java/com/github/lucasdevrj/mvc/leitura/ConfiguracaoSegurancaWeb.java
@@ -1,9 +1,14 @@
 package com.github.lucasdevrj.mvc.leitura;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration //Indica para Spring que esta é uma classe de configuração
 @EnableWebSecurity
@@ -14,6 +19,22 @@ public class ConfiguracaoSegurancaWeb extends WebSecurityConfigurerAdapter { //C
 		http.authorizeHttpRequests()
 			.anyRequest().authenticated()
 			.and()
-			.httpBasic();
+			.formLogin((form) -> form
+				.loginPage("/login")
+				.permitAll()
+			);
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() { //método para criar usuário
+		UserDetails user =
+			 User.withDefaultPasswordEncoder()
+				.username("lucas")
+				.password("abr33")
+				.roles("ADM")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 }
